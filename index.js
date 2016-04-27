d782f48ad65d0fe7b6ef481f9c3b78e383769867
highdutch <dick.heijink@gmail.com>
2016-04-27 23:51:31 +0200

    es6

--- index.js
+++ index.js
@@ -1,165 +1,141 @@
 "use strict";
 /*global Buffer,process*/
 
-var fs = require('fs');
-var path = require('path');
-var stream = require('stream');
+const fs = require('fs');
+const path = require('path');
+const stream = require('stream');
 
-var es = require('event-stream');
-var extfs = require('fs-extra');
-var semver = require('semver');
-var vfs = require('vinyl-fs');
-var yauzl = require('yauzl');
-var yazl = require('yazl');
+const es = require('event-stream');
+const extfs = require('fs-extra');
+const semver = require('semver');
+const vfs = require('vinyl-fs');
+const yauzl = require('yauzl');
+const yazl = require('yazl');
 
-var util = module.exports = {};
+const util = module.exports = {};
 
-function promiseCallback(resolve, reject, err, result) {
-  if (err) { reject(err); } else { resolve(result); }
+function promiseCallback(resolve, reject) {
+  return (err, result) => err ? reject(err) : resolve(result)
 }
 
 function denodeify(f) {
-  return function () {
-    var args = Array.prototype.slice.call(arguments);
-    return new Promise(function (resolve, reject) {
-      args.push(promiseCallback.bind(null, resolve, reject));
-      f.apply(null, args);
+  return function() {
+    const args = [...arguments];
+    return new Promise((resolve, reject) => {
+      args.push(promiseCallback(resolve, reject));
+      f(...args);
     });
   };
 }
 
 util.bestVersion = semver.maxSatisfying;
 
-util.compareVersions = function (v1, v2) {
-  return semver.gt(v1, v2) ? 1 : semver.lt(v1, v2) ? -1 : 0;
-}
+util.compareVersions = (v1, v2) => semver.gt(v1, v2) ? 1 : semver.lt(v1, v2) ? -1 : 0;
 
-util.copy = function (promisedInput, promisedOutput) {
-  return Promise.all([promisedInput, promisedOutput])
-    .then(function (io) {
-      var input = io[0], output = io[1];
-      return new Promise(function (resolve, reject) {
-        output.once('error', reject);
-        input.once('error', reject).once('end', resolve).pipe(output);
-      });
-    })
-    ;
-};
+util.copy = (promisedInput, promisedOutput) => Promise.all([promisedInput, promisedOutput])
+  .then(io => {
+    const input = io[0], output = io[1];
+    return new Promise((resolve, reject) => {
+      output.once('error', reject);
+      input.once('error', reject).once('end', resolve).pipe(output);
+    });
+  })
+  ;
 
-util.doNothing = function () { };
+util.copyJSON = (promisedJSON, promisedOutput) => Promise.resolve(promisedJSON)
+  .then(json => util.copy(util.streamInput(JSON.stringify(json, null, ' ')), promisedOutput))
+  ;
 
-util.endsWith = function (s, postfix) {
-  return postfix.length <= s.length && s.lastIndexOf(postfix) === (s.length - postfix.length);
-};
+util.doNothing = () => { };
 
-util.fileExtensions = function (categories, selection) {
-  var names = selection ? selection.split(',') : Object.getOwnPropertyNames(categories);
-  var extensions = [];
-  names.forEach(function (name) {
-    Array.prototype.push.apply(extensions, (categories[name] || name).split(','));
-  });
+util.eachFile = (files, fn) => new Promise((resolve, reject) => {
+  vfs.src(files, { allowEmpty: true }).pipe(es.map((file, cb) => {
+    Promise.resolve(fn(file)).then(() => cb(null));
+  })).once('error', reject).once('end', resolve);
+});
+
+util.endsWith = (s, postfix) =>
+  postfix.length <= s.length && s.lastIndexOf(postfix) === (s.length - postfix.length);
+
+util.fileExtensions = (categories, selection) => {
+  const extensions = [];
+  for (let name of selection ? selection.split(',') : Object.keys(categories)) {
+    extensions.push(...(categories[name] || name).split(','));
+  }
   return extensions;
 };
 
-util.filesPattern = function (categories, files) {
+util.filesPattern = (categories, files) => {
   if (typeof files === 'string') {
     return files;
   }
-  var home = files.home || '.';
-  var base = files.base || '*';
-  var extensions = files.category ? util.fileExtensions(categories, files.category) : null;
-  var extension;
-  if (!extensions) {
-    extension = '*'
-  } else if (extensions.length === 1) {
-    extension = extensions[0];
-  } else {
-    extension = '@(' + extensions.join('|') + ')';
-  }
+  const home = files.home || '.', base = files.base || '*';
+  const extensions = files.category ? util.fileExtensions(categories, files.category) : null;
+  const extension = !extensions ? '*' : extensions.length === 1 ? extensions[0] :
+    `@(${extensions.join('|')})`;
   return home + '/' + base + '.' + extension;
 };
 
-util.hasEnumerables = function (o) {
-  for (var _ in o) { return true; }
+util.hasEnumerables = o => {
+  for (let _ in o) { return true; }
   return false;
-};
-
-util.mapFiles = function (files, fn) {
-  return new Promise(function (resolve, reject) {
-    vfs.src(files).pipe(es.map(fn)).once('error', reject).once('end', resolve);
-  });
 };
 
 util.mkdir = denodeify(extfs.mkdirs);
 
-util.openReadStream = function (filePath, fileOpts) {
-  return new Promise(function (resolve, reject) {
-    if (!filePath) {
-      resolve(process.stdin);
-    } else {
-      fs.createReadStream(filePath, fileOpts).once('error', reject)
-        .once('open', function () { resolve(this); });
-    }
-  });
-};
-
-util.openWriteStream = function (filePath, fileOpts) {
-  return new Promise(function (resolve, reject) {
-    if (!filePath) {
-      resolve(process.stdout);
-    } else {
-      util.mkdir(path.dirname(filePath))
-        .then(function () {
-          fs.createWriteStream(filePath, fileOpts).once('error', reject)
-            .once('open', function () { resolve(this); });
-        })
-      ;
-    }
-  });
-};
-
-util.readFileText = function (vfile, withCrs) {
-  var contents = vfile.contents;
-  if (vfile.isStream()) {
-    return util.readStreamText(contents, withCrs);
+util.openReadStream = (filePath, fileOpts) => new Promise((resolve, reject) => {
+  if (!filePath) {
+    resolve(process.stdin);
   } else {
-    var result = vfile.isBuffer() ? contents.toString() : contents;
-    return Promise.resolve(result);
+    fs.createReadStream(filePath, fileOpts)
+      .once('error', reject).once('open', function() { resolve(this); });
   }
+});
+
+util.openWriteStream = (filePath, fileOpts) => new Promise((resolve, reject) => {
+  if (!filePath) {
+    resolve(process.stdout);
+  } else {
+    util.mkdir(path.dirname(filePath))
+      .then(() => {
+        fs.createWriteStream(filePath, fileOpts)
+          .once('error', reject).once('open', function() { resolve(this); });
+      })
+      ;
+  }
+});
+
+util.readFileText = (vfile, withCrs) => {
+  const contents = vfile.contents;
+  return vfile.isStream() ? util.readStreamText(contents, withCrs) :
+    Promise.resolve(vfile.isBuffer() ? contents.toString() : contents);
 };
 
-util.readStreamBuffer = function (input) {
-  return util.readStreamChunks(input)
-    .then(function (chunks) { return Buffer.concat(chunks); })
-    ;
-};
+util.readStreamBuffer = input => util.readStreamChunks(input)
+  .then(chunks => Buffer.concat(chunks))
+  ;
 
-util.readStreamChunks = function (input) {
-  return new Promise(function (resolve, reject) {
-    var chunks = [];
-    input
-      .on('data', function (chunk) { chunks.push(chunk); })
-      .on('error', reject)
-      .on('end', function () { resolve(chunks); });
-  });
-};
+util.readStreamChunks = input => new Promise((resolve, reject) => {
+  const chunks = [];
+  input.on('error', reject)
+    .on('data', chunk => { chunks.push(chunk); })
+    .on('end', () => { resolve(chunks); });
+});
 
-util.readStreamText = function (input, withCrs) {
+util.readStreamText = (input, withCrs) => {
   input.setEncoding('utf8');
   return util.readStreamChunks(input)
-    .then(function (chunks) {
-      var text = chunks.join('');
-      return withCrs ? text : text.replace(/\r/g, '');
-    })
+    .then(chunks => chunks.join(''))
+    .then(text => withCrs ? text : text.replace(/\r/g, ''))
     ;
 };
 
 util.rmdir = denodeify(extfs.remove);
 
-util.selectEntries = function (entries, prefix, postfix) {
-  var selected = {};
+util.selectEntries = (entries, prefix, postfix) => {
   postfix = postfix || '';
-  for (var key in entries) {
+  const selected = {};
+  for (let key in entries) {
     if (util.startsWith(key, prefix) && util.endsWith(key, postfix)) {
       selected[key.substring(prefix.length, key.length - postfix.length)] = entries[key];
     }
@@ -169,87 +145,74 @@ util.selectEntries = function (entries, prefix, postfix) {
 
 util.seps = new RegExp('\\' + path.sep, 'g');
 
-util.startsWith = function (s, prefix) {
-  return s.indexOf(prefix) === 0;
-};
+util.startsWith = (s, prefix) => s.indexOf(prefix) === 0;
 
 util.stat = denodeify(fs.stat);
 
-util.streamInput = function () {
-  var chunks = Array.prototype.slice.call(arguments);
-  var input = new stream.Readable();
-  input._read = function () {
-    chunks.forEach(function (chunk) { input.push(chunk); });
+util.streamInput = function() {
+  const chunks = [...arguments];
+  const input = new stream.Readable();
+  input._read = () => {
+    for (let chunk of chunks) {
+      input.push(chunk);
+    }
     input.push(null);
   };
   return input;
 };
 
-var openZip = denodeify(yauzl.open);
-util.unzip = function (filePath) {
-  var zip = { entries: {} };
-  return openZip(filePath, { autoClose: false })
-    .then(function (yauzFile) {
-      zip.file = yauzFile;
-      return new Promise(function (resolve, reject) {
-        yauzFile.once('error', reject).once('end', function () { resolve(zip); })
-          .on('entry', function (entry) {
-            var fileName = entry.fileName;
-            if (zip.entries[fileName]) {
-              throw new Error('Duplicate ' + fileName + ' in ' + filePath);
-            }
-            zip.entries[fileName] = entry;
-          });
+const openZip = denodeify(yauzl.open);
+util.unzip = filePath => openZip(filePath, { autoClose: false })
+  .then(yauzFile => new Promise((resolve, reject) => {
+    const zip = { entries: {}, file: yauzFile };
+    yauzFile.once('error', reject).once('end', () => resolve(zip))
+      .on('entry', entry => {
+        const fileName = entry.fileName;
+        if (zip.entries[fileName]) {
+          throw new Error(`Duplicate ${fileName} in ${filePath}`);
+        }
+        zip.entries[fileName] = entry;
       });
-    })
-    ;
-};
+  }))
+  ;
 
-util.unzipDirectory = function (yauzFile, entries, targetDir) {
-  var completions = [];
-  for (var entryPath in entries) {
-    var filePath = path.resolve(targetDir, entryPath);
-    completions.push(util.unzipFile(yauzFile, entries[entryPath], filePath));
-  }
-  return Promise.all(completions).then(util.doNothing);
-};
-
-util.unzipFile = function (yauzFile, entry, filePath) {
-  return util.copy(util.unzipStream(yauzFile, entry), util.openWriteStream(filePath));
-};
-
-util.unzipBuffer = function (yauzFile, entry) {
-  return util.unzipStream(yauzFile, entry)
-    .then(function (input) { return util.readStreamBuffer(input); })
-    ;
-};
-
-util.unzipStream = function (yauzFile, entry) {
-  return new Promise(function (resolve, reject) {
-    yauzFile.openReadStream(entry, promiseCallback.bind(null, resolve, reject));
+util.unzipDirectory = (yauzFile, entries, targetDir) => {
+  const files = Object.keys(entries).map(entryPath => {
+    const filePath = path.resolve(targetDir, entryPath);
+    return util.unzipFile(yauzFile, entries[entryPath], filePath)
   });
+  return Promise.all(files).then(util.doNothing);
 };
 
-util.unzipText = function (yauzFile, entry, withCrs) {
-  return util.unzipStream(yauzFile, entry)
-    .then(function (input) { return util.readStreamText(input, withCrs); })
-    ;
-};
+util.unzipFile = (yauzFile, entry, filePath) =>
+  util.copy(util.unzipStream(yauzFile, entry), util.openWriteStream(filePath));
+
+util.unzipBuffer = (yauzFile, entry) => util.unzipStream(yauzFile, entry)
+  .then(input => util.readStreamBuffer(input))
+  ;
+
+util.unzipStream = (yauzFile, entry) => new Promise((resolve, reject) => {
+  yauzFile.openReadStream(entry, promiseCallback(resolve, reject));
+});
+
+util.unzipText = (yauzFile, entry, withCrs) => util.unzipStream(yauzFile, entry)
+  .then(input => util.readStreamText(input, withCrs))
+  ;
 
 util.vseps = /\//g;
 
-util.zip = function (output) {
-  var yazFile = new yazl.ZipFile();
+util.zip = output => {
+  const yazFile = new yazl.ZipFile();
   yazFile.outputStream.pipe(output);
   return yazFile;
 };
 
-util.zipBuffer = function (yazFile, relative, buffer, mtime, plain) {
+util.zipBuffer = (yazFile, relative, buffer, mtime, plain) => {
   yazFile.addBuffer(buffer, relative, { mtime: mtime, compress: !plain });
 };
 
-util.zipFile = function (yazFile, relative, vfile, plain) {
-  var options = { mtime: vfile.stat.mtime, mode: vfile.stat.mode, compress: !plain };
+util.zipFile = (yazFile, relative, vfile, plain) => {
+  const options = { mtime: vfile.stat.mtime, mode: vfile.stat.mode, compress: !plain };
   if (vfile.isBuffer()) {
     yazFile.addBuffer(vfile.contents, relative, options);
   } else if (vfile.isStream()) {
@@ -257,6 +220,6 @@ util.zipFile = function (yazFile, relative, vfile, plain) {
   }
 };
 
-util.zipStream = function (yazFile, relative, input, mtime, plain) {
+util.zipStream = (yazFile, relative, input, mtime, plain) => {
   yazFile.addReadStream(input, relative, { mtime: mtime, compress: !plain });
 };
